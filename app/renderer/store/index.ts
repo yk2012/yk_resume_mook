c54679d077480ed1f9af9e827d6d26b7136765cd
ykang2020 <yk1123@vip.163.com>
2022-04-16 15:56:05 +0800

    redux

--- app/renderer/store/index.ts
+++ app/renderer/store/index.ts
@@ -1,11 +1,12 @@
 import logger from 'redux-logger'
 import RcReduxModel from 'rc-redux-model'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
-
+// 引入 model
 import globalModel from './globalModel'
-
+// 得到 reduxModel
 const reduxModel = new RcReduxModel([globalModel])
 
+// 无侵入式的使用redux
 const reducerList = combineReducers(reduxModel.reducers)
-
+// 导出创建的store
 export default createStore(reducerList, applyMiddleware(reduxModel.thunk, logger))
